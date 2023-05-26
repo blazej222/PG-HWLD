0c13f279f8105803a7e7a427ae4266a9084b837a
blazej222 <firma.bio@gmail.com>
2023-05-26 20:13:48 +0200

    Add debug print to feedforward, changed some parameters

--- models/FeedForward/main.py
+++ models/FeedForward/main.py
@@ -1,8 +1,20 @@
 import feedforwardNN
+#from timeit import default_timer as timer
+import time
+
+# train_path = "../../resources/datasets/unpacked/dataset-emnist/train-images"
+# test_path = "../../resources/datasets/unpacked/dataset-emnist/test-images"
+
+train_path = "C:/tmp/dataset-emnist/train-images"
+test_path = "C:/tmp/dataset-emnist/test-images"
 
 if __name__ == '__main__':
     fnn = feedforwardNN.FNN()
-    fnn.load_sets("../../resources/datasets/unpacked/dataset-emnist/train-images", True)
-    fnn.load_sets("../../resources/datasets/unpacked/dataset-emnist/test-images", False)
+    fnn.load_sets(train_path, True)
+    fnn.load_sets(test_path, False)
+    print("Starting training process")
+    start = time.time()
     fnn.train()
+    end = time.time()
+    print("Training finished in {}".format(end-start))
     predictions = fnn.predict()
