f2b3c587dc8c6f6783423227666e867f8ec85ae7
blazej222 <firma.bio@gmail.com>
2023-05-27 20:17:52 +0200

    Implement multithreading

--- models/pytesseract/main.py
+++ models/pytesseract/main.py
@@ -1,8 +1,9 @@
 import pytesseract
 from PIL import Image
 import os
+import multiprocessing as mp
 tesseractPath = "./bin/tesseract.exe"
-dataset_test = "../../resources/datasets/unpacked/dataset-emnist/test-images" # temporary test path
+dataset_test = "../../resources/datasets/unpacked/dataset-klaudia/Testing" # temporary test path
 
 pytesseract.pytesseract.tesseract_cmd = tesseractPath
 
@@ -12,24 +13,44 @@ flawlessly_recongized = 0
 ok_recognized = 0
 dataset_size = 0
 
-for dir in directories:
-    files = sorted(os.listdir(os.path.join(dataset_test, dir)))  # contains files
-    for file in files:
-        dataset_size += 1
-        fullpath = os.path.join(dataset_test, dir, file)
-        #  Refer to https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc
-        letter = pytesseract.image_to_string(fullpath, config='--psm 10 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
-        #  letter = pytesseract.image_to_string(Image.open(fullpath), config='--psm 10 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz')
-        #  To recognize between uppercase and lowercase, add ABCDEFGHIJKLMNOPQRSTUVWXYZ to whitelist
-        letter = letter.replace("\n","") #  get rid of the newline character
+def recognize(file,dir):
+    current_flawlessly_recongized = 0
+    current_ok_recognized = 0
+    #current_dataset_size = len(files)
+    #for file in files:
+    fullpath = os.path.join(dataset_test, dir, file)
+    #  Refer to https://github.com/tesseract-ocr/tesseract/blob/main/doc/tesseract.1.asc
+    letter = pytesseract.image_to_string(fullpath, config='--psm 10 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    #  letter = pytesseract.image_to_string(Image.open(fullpath), config='--psm 10 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz')
+    #  To recognize between uppercase and lowercase, add ABCDEFGHIJKLMNOPQRSTUVWXYZ to whitelist
+    letter = letter.replace("\n","") #  get rid of the newline character
 
-        if len(letter) == 1 and (letter == dir or letter == dir.upper()): flawlessly_recongized += 1  #  if we recognized one letter and its the correct one
-        elif len(letter) > 1:
-            if letter[0] == dir or letter[0] == dir.upper(): ok_recognized += 1
-        else:
-            pass
-    print("Currently done directory {}".format(dir))
+    if len(letter) == 1 and (letter == dir or letter == dir.upper()): current_flawlessly_recongized += 1  #  if we recognized one letter and its the correct one
+    elif len(letter) > 1:
+        if letter[0] == dir or letter[0] == dir.upper(): current_ok_recognized += 1
+    else:
+        pass
+    # print("Image {}/{} recognized as {}".format(dir,file,letter))
+    return [current_flawlessly_recongized,current_ok_recognized]
 
-        #print("Image {}/{} recognized as {}".format(dir,file,letter))
-percentage = (flawlessly_recongized + ok_recognized) / dataset_size
-print("Percentage was {}".format(percentage))
+if __name__ == '__main__':
+
+    for dir in directories:
+        files = sorted(os.listdir(os.path.join(dataset_test, dir)))  # contains files
+        pool = mp.Pool()
+        processmap = pool.starmap_async(recognize,[(file,dir) for file in files]) # Async currently works only on directory level
+
+        pool.close()
+        pool.join()
+
+        results = processmap.get()
+        dataset_size += len(files)
+
+        for b,c in results:
+            flawlessly_recongized += b
+            ok_recognized += c
+        print("Done directory {}".format(dir))
+
+
+    percentage = (flawlessly_recongized + ok_recognized) / dataset_size
+    print("Percentage was {}".format(percentage))
