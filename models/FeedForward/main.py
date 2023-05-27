1afba6acaf20edf9e2744c57b48369125a8c12c7
blazej222 <firma.bio@gmail.com>
2023-05-27 12:50:18 +0200

    Added more debugprints, feedforward currently experimenting with more epochs, added one more layer to feedforward

--- models/FeedForward/main.py
+++ models/FeedForward/main.py
@@ -2,16 +2,20 @@ import feedforwardNN
 #from timeit import default_timer as timer
 import time
 
-# train_path = "../../resources/datasets/unpacked/dataset-emnist/train-images"
-# test_path = "../../resources/datasets/unpacked/dataset-emnist/test-images"
+train_path = "../../resources/datasets/unpacked/dataset-emnist/train-images"
+test_path = "../../resources/datasets/unpacked/dataset-emnist/test-images"
 
-train_path = "C:/tmp/dataset-emnist/train-images"
-test_path = "C:/tmp/dataset-emnist/test-images"
+#train_path = "C:/tmp/dataset-emnist/train-images"
+#test_path = "C:/tmp/dataset-emnist/test-images"
 
 if __name__ == '__main__':
     fnn = feedforwardNN.FNN()
+    print("Started loading")
+    start = time.time()
     fnn.load_sets(train_path, True)
     fnn.load_sets(test_path, False)
+    end = time.time()
+    print("Loading finished in {}".format(end - start))
     print("Starting training process")
     start = time.time()
     fnn.train()
