8bee6f85dd1fea0e0b0e1f7ed349aab200dc3324
blazej222 <firma.bio@gmail.com>
2023-05-28 20:03:18 +0200

    Added MP to test loop in CNN

--- models/cnn/neural_network.py
+++ models/cnn/neural_network.py
@@ -8,10 +8,40 @@ from keras.layers import Dense, Flatten, MaxPooling2D, Conv2D
 from keras.preprocessing.image import ImageDataGenerator
 from keras.saving.legacy.model_config import model_from_json
 from keras.utils import load_img, img_to_array
+import multiprocessing as mp
 
 import sys  # required to make imports from another directory work
 #sys.path.append('../../utils')
 from utils.DataSetConverter import main as dsc
+
+def test_file( catalog, file, isLabelFile, test_by_folder, labelArray, label, doPrint,cnn,i):
+    correctCount = 0
+    incorrectCount = 0
+    tmpath = os.path.join(catalog, file)
+    tmpath = os.path.normpath(tmpath)
+    img = load_img(tmpath, target_size=(28, 28))
+    imgArray = img_to_array(img)
+    imgArray = np.expand_dims(imgArray, axis=0)
+    imgArray = np.vstack([imgArray])
+    predictedLetter = cnn.predict(imgArray, verbose=0)
+    predictedLetter = find_letter(predictedLetter)
+    if isLabelFile:
+        actualLetter = chr(labelArray[i] + 97)
+    elif test_by_folder:
+        actualLetter = ntpath.basename(catalog)[0]
+    else:
+        actualLetter = label
+
+    if doPrint:
+        print("Predicted: " + predictedLetter + " Actual: " + actualLetter)
+
+    if predictedLetter == actualLetter:
+        correctCount += 1
+    else:
+        # img.show()
+        incorrectCount += 1
+
+    return [correctCount, incorrectCount]
 
 def find_letter(letter):
     max_prob = 0
@@ -83,11 +113,24 @@ class Network:
             json_file.write(model_json)
         self.cnn.save_weights("cnn.h5")
 
+
+
+    def test(self, fileName):
+        img = load_img(fileName)
+        img = img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = np.vstack([img])
+        predictedLetter = self.cnn.predict(img, verbose=0)
+        predictedLetter = find_letter(predictedLetter)
+        print("Predicted letter: " + predictedLetter)
+
+    #  TODO: Check if there's a cleaner way to implement this
     def test_catalog(self, catalog, labelFileName=None, label=None, doPrint=False, test_by_folder=False):
         i = 0
         correctCount = 0
         incorrectCount = 0
         isLabelFile = None
+        labelArray = 0
 
         files = sorted(os.listdir(catalog), key=len)
 
@@ -102,40 +145,15 @@ class Network:
 
         print("Starting catalog tests")
 
-        for file in files:
-            tmpath = os.path.join(catalog, file)
-            tmpath = os.path.normpath(tmpath)
-            img = load_img(tmpath, target_size=(28, 28))
-            imgArray = img_to_array(img)
-            imgArray = np.expand_dims(imgArray, axis=0)
-            imgArray = np.vstack([imgArray])
-            predictedLetter = self.cnn.predict(imgArray, verbose=0)
-            predictedLetter = find_letter(predictedLetter)
-            if isLabelFile:
-                actualLetter = chr(labelArray[i] + 97)
-            elif test_by_folder:
-                actualLetter = ntpath.basename(catalog)[0]
-            else:
-                actualLetter = label
+        pool = mp.Pool()
+        process = pool.starmap_async(test_file,[(catalog,x,isLabelFile,test_by_folder,labelArray,label,doPrint,self.cnn,files.index(x)) for x in files])
+        pool.close()
+        pool.join()
 
-            if doPrint:
-                print("Predicted: " + predictedLetter + " Actual: " + actualLetter)
+        result = process.get()
 
-            if predictedLetter == actualLetter:
-                correctCount += 1
-            else:
-                # img.show()
-                incorrectCount += 1
-
-            i += 1
+        for a,b in result:
+            correctCount += a
+            incorrectCount += b
 
         print("Accuracy: " + str(correctCount / (correctCount + incorrectCount)))
-
-    def test(self, fileName):
-        img = load_img(fileName)
-        img = img_to_array(img)
-        img = np.expand_dims(img, axis=0)
-        img = np.vstack([img])
-        predictedLetter = self.cnn.predict(img, verbose=0)
-        predictedLetter = find_letter(predictedLetter)
-        print("Predicted letter: " + predictedLetter)
