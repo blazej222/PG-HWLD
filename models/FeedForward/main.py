63b5ee922ddf41dd56a6932cc1951e0e6bb4f145
Klaudia <klaudia.ratkowska.one@wp.pl>
2023-06-05 00:27:16 +0200

    feedforward final version

--- models/FeedForward/main.py
+++ models/FeedForward/main.py
@@ -2,7 +2,10 @@ import feedforwardNN
 
 if __name__ == '__main__':
     fnn = feedforwardNN.FNN()
-    fnn.load_sets("train-images", True)
-    fnn.load_sets("test-images", False)
+    fnn.load_sets("train-augmented-images", True)
+    fnn.load_sets("test-augmented-images", False)
     fnn.train()
-    predictions = fnn.predict(-1, "test-images")
+    predictions = fnn.predict(-1, "test-augmented-images")
+
+
+
