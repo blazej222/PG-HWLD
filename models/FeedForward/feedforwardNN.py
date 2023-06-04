f4d9f59fca77af7e96fec61ebe0b11891f55bda5
Klaudia <klaudia.ratkowska.one@wp.pl>
2023-06-04 14:04:47 +0200

    added loss

--- models/FeedForward/feedforwardNN.py
+++ models/FeedForward/feedforwardNN.py
@@ -2,6 +2,7 @@ import os
 from tkinter import Image
 from PIL import Image
 import numpy as np
+from matplotlib import pyplot
 
 
 def sigmoid(x):
@@ -9,6 +10,12 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximum(0, x)
+
+
+def softmax(values):
+    exp_values = np.exp(values)
+    exp_values_sum = np.sum(exp_values)
+    return exp_values / exp_values_sum
 
 
 class FNN:
@@ -20,7 +27,7 @@ class FNN:
         self.w3 = None
         self.b3 = None
         self.error = None
-        self.epochs = 6000
+        self.epochs = 7000
         self.h1_output = None
         self.h2_output = None
         self.predicted_output = None
@@ -30,8 +37,18 @@ class FNN:
         self.test_set = []
         self.current_set = None
         self.learning_rate = 0.00001
+        self.accuracy_arr = []
+        self.epoch_arr = []
+        self.loss = []
 
     def initialize_parameters(self):
+        if len(self.train_labels) <= 10000:
+            self.learning_rate = 0.00001
+        elif 10000 < len(self.train_labels) <= 100000:
+            self.learning_rate = 0.000001
+        else:
+            self.learning_rate = 0.0000001
+
         input_size = self.train_set.shape[1]
         h1_size = 128
         h2_size = 64
@@ -62,27 +79,59 @@ class FNN:
         self.w1 += self.learning_rate * np.dot(self.train_set.T, h1_error)
         self.b1 += self.learning_rate * np.sum(h1_error, axis=0)
 
+        self.cross_entropy()
+
     def train(self):
         self.current_set = self.train_set
         for epoch in range(self.epochs):
             self.forward()
             self.backward()
             if epoch % 100 == 0:
-                self.predict()
-                print(epoch)
-                # tu zrobic wykres
+                print("Epoch: ", epoch)
+                self.predict(epoch, None)
 
-    def predict(self):
-        accuracy = 0
-        self.current_set = self.test_set
-        self.forward()
-        predicted_labels = np.argmax(self.predicted_output, axis=1)
-        for i in range(len(predicted_labels)):
-            if predicted_labels[i] == self.test_labels[i]:
-                accuracy += 1
-        print("Accuracy: ", accuracy / len(predicted_labels))
+    def cross_entropy(self):
+        if np.array_equal(self.current_set, self.train_set):
+            labels = self.train_labels
+        else:
+            labels = self.test_labels
 
-        self.current_set = self.train_set
+        softmax(self.predicted_output)
+        loss = 0
+        for i in range(len(self.predicted_output)):
+            loss += -1 * labels[i] * np.log(self.predicted_output[i])
+        total_loss = loss.sum()
+        self.loss.append(total_loss)
+
+    def predict(self, epoch, dir_name):
+        if epoch == -1:
+            pyplot.plot(self.epoch_arr, self.accuracy_arr)
+            pyplot.ylabel("Accuracy")
+            pyplot.xlabel("Epoch")
+            pyplot.grid()
+            pyplot.title("Model accuracy for set " + dir_name)
+            pyplot.savefig(dir_name + "_accuracy_plot.png")
+
+            pyplot.plot(self.loss)
+            pyplot.ylabel("Loss")
+            pyplot.xlabel("Epoch")
+            pyplot.grid()
+            pyplot.title("Model loss for set " + dir_name)
+            pyplot.savefig(dir_name + "_loss_plot.png")
+
+        else:
+            correct = 0
+            self.current_set = self.test_set
+            self.forward()
+            predicted_labels = np.argmax(self.predicted_output, axis=1)
+            for i in range(len(predicted_labels)):
+                if predicted_labels[i] == self.test_labels[i]:
+                    correct += 1
+            accuracy = correct/len(predicted_labels)
+            print("Accuracy: ", accuracy)
+            self.accuracy_arr.append(accuracy)
+            self.epoch_arr.append(epoch)
+            self.current_set = self.train_set
 
     def load_sets(self, directory, is_train_set):
         imagesArr = []
