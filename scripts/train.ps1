50d17c61f142a98c018194c4e5cf1da0a5bb75f3
LotusPenguin <125930228+LotusPenguin@users.noreply.github.com>
2024-09-06 01:33:16 +0200

    added wavemix to train.ps1

--- scripts/train.ps1
+++ scripts/train.ps1
@@ -1,17 +1,26 @@
 # Train VGG on default dataset thrice
 
 ../resources/python/venv/Scripts/activate.ps1
-cd ../models/vgg
+cd ../models/WaveMix
 python main.py --saved_model_path ./saved_models/run1
 python main.py --saved_model_path ./saved_models/run2
 python main.py --saved_model_path ./saved_models/run3
 cd ../..
+
+cd models/vgg
+python main.py --saved_model_path ./saved_models/run1
+python main.py --saved_model_path ./saved_models/run2
+python main.py --saved_model_path ./saved_models/run3
+
+# cd ../..
+# deactivate
+
+# # Train TextCaps on default dataset thrice
+
+# resources/python/venv-3.6.8/Scripts/activate.ps1
+# cd models/TextCaps
+# python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run1 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
+# python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run2 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
+# python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run3 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
+cd ../../scripts
 deactivate
-
-# Train TextCaps on default dataset thrice
-
-resources/python/venv-3.6.8/Scripts/activate.ps1
-cd models/TextCaps
-python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run1 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
-python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run2 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
-python textcaps_emnist_bal.py --cnt 5600 --save_dir ./saved_models/emnist_letters_5600_run3 --train_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat --test_path ../../resources/datasets/dataset-EMNIST-mat/emnist-letters.mat
